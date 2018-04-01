0858d365b17492a1fa306ab00593f2e5f9e00265
heihong <traingheihong@gmail.com>
2018-04-01 20:10:35 +0200

    start_refacto

--- javascript/app/exo1/matcher.js
+++ javascript/app/exo1/matcher.js
@@ -1,21 +1,38 @@
-function Matcher() {
-	this.match = match;
-	
-	function match(expected, actual, clipLimit, delta){
-		for (let i = 0; i < actual.length; i++)
-			if (actual[i] > clipLimit)
-				actual[i] = clipLimit;
-		
-		//Check for length differences
-		if (actual.length != expected.length)
-			return false;
-		
-		//Check that each entry within the expected +/- delta
-		for (let i = 0; i < expected.length; i++)
-			if (Math.abs(expected[i] - actual[i]) > delta)
-				return false;
-		
-		return true;
-	}
+"use strict"
+
+class Matcher {
+
+    constructor(){
+
+    }
+
+
+    match(expected, actual, clipLimit, delta){
+        for (let i = 0; i < actual.length; i++)
+            if (this.isSuperior(actual[i], clipLimit))
+                actual[i] = clipLimit;
+
+        //Check for length differences
+        if (!this.isEqual(actual.length, expected.length))
+            return false;
+
+        //Check that each entry within the expected +/- delta
+        for (let i = 0; i < expected.length; i++) {
+            let nbAbs = Math.abs(expected[i] - actual[i]);
+            if (this.isSuperior(nbAbs, delta)){
+                return false;
+            }
+
+        }
+        return true;
+    }
+
+    isSuperior(nbAbs, delta) {
+        return nbAbs > delta;
+    }
+
+    isEqual(actual, expected) {
+        return actual == expected;
+    }
 }
 module.exports = Matcher;
